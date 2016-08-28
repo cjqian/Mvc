fcc7797c8f67539634bd5e032215482310ab60ee
Crystal Qian <crystaljqian@gmail.com>
2016-08-27 17:42:02 -0700

    Added original files.

---
+++ src/Microsoft.AspNetCore.Mvc.Razor/CompositeTagHelperDescriptorResolver.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Compilation.TagHelpers;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNetCore.Mvc.Razor
+{
+    public class CompositeTagHelperDescriptorResolver : ITagHelperDescriptorResolver
+    {
+        public IList<TagHelperDescriptorResolver> Resolvers { get; set; }
+        public CompositeTagHelperDescriptorResolver()
+        {
+            Resolvers = new List<TagHelperDescriptorResolver>();
+        }
+
+        public IEnumerable<TagHelperDescriptor> Resolve(TagHelperDescriptorResolutionContext resolutionContext)
+        {
+            var descriptors = new List<TagHelperDescriptor>();
+
+            foreach (var resolver in Resolvers)
+            {
+                var currentDescriptors = resolver.Resolve(resolutionContext);
+                descriptors.AddRange(currentDescriptors);
+            }
+
+            return descriptors;
+        }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Mvc.Razor/Internal/ViewComponentTagHelperDescriptorResolver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.Compilation.TagHelpers;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.Internal
+{
+    public class ViewComponentTagHelperDescriptorResolver : TagHelperDescriptorResolver
+    {
+        ViewComponentTagHelperDescriptorFactory _descriptorFactory;
+
+        public ViewComponentTagHelperDescriptorResolver(
+            IViewComponentDescriptorProvider viewComponentDescriptorProvider)
+            : base(designTime: true)
+        {
+            _descriptorFactory = new ViewComponentTagHelperDescriptorFactory(viewComponentDescriptorProvider);
+        }
+
+        public new IEnumerable<TagHelperDescriptor> Resolve(TagHelperDescriptorResolutionContext resolutionContext)
+        {
+            return base.Resolve(resolutionContext);
+        }
+
+        protected override IEnumerable<TagHelperDescriptor> ResolveDescriptorsInAssembly(
+            string assemblyName,
+            SourceLocation documentLocation,
+            ErrorSink errorSink)
+        {
+            var tagHelperDescriptors = base.ResolveDescriptorsInAssembly(
+                assemblyName,
+                documentLocation,
+                errorSink);
+
+            var viewComponentTagHelperDescriptors = _descriptorFactory.CreateDescriptors(assemblyName);
+            var descriptors = tagHelperDescriptors.Concat(viewComponentTagHelperDescriptors);
+            return descriptors;
+        }
+    }
+}
