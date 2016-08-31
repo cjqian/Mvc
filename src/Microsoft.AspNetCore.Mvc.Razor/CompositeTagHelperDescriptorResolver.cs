5c8f24074018286cfee4b139e5772bbaa84f2042
Crystal Qian <crystaljqian@gmail.com>
2016-08-31 12:40:32 -0700

    Initial commit.

---
+++ src/Microsoft.AspNetCore.Mvc.Razor/CompositeTagHelperDescriptorResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Compilation.TagHelpers;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNetCore.Mvc.Razor
+{
+    public class CompositeTagHelperDescriptorResolver : ITagHelperDescriptorResolver
+    {
+        public IList<TagHelperDescriptorResolver> Resolvers { get; set; }
+        public CompositeTagHelperDescriptorResolver()
+        {
+            Resolvers = new List<TagHelperDescriptorResolver>();
+        }
+
+        public IEnumerable<TagHelperDescriptor> Resolve(TagHelperDescriptorResolutionContext resolutionContext)
+        {
+            var descriptors = new List<TagHelperDescriptor>();
+
+            foreach (var resolver in Resolvers)
+            {
+                var currentDescriptors = resolver.Resolve(resolutionContext);
+                descriptors.AddRange(currentDescriptors);
+            }
+
+            return descriptors;
+        }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Mvc.Razor/Internal/DefaultCompositeTagHelperDescriptorResolver.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNetCore.Mvc.Razor.Internal
+{
+    public class DefaultCompositeTagHelperDescriptorResolver : CompositeTagHelperDescriptorResolver
+    {
+        public DefaultCompositeTagHelperDescriptorResolver(
+            TagHelperDescriptorResolver resolver1,
+            ViewComponentTagHelperDescriptorResolver resolver2)
+            : base()
+        {
+            Resolvers.Add(resolver1);
+            Resolvers.Add(resolver2);
+        }
+    }
+}
